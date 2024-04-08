cc24d82786235a27c3984a46d77ea6893f20dd96
MelzCodes <matteo.melis2012@gmail.com>
2024-04-08 23:04:44 +0100

    Forgot to dynamically update constraints

--- src/components/Workspace/ProblemDescription/ProblemDescription.tsx
+++ src/components/Workspace/ProblemDescription/ProblemDescription.tsx
@@ -77,20 +77,8 @@ const ProblemDescription: React.FC<ProblemDescriptionProps> = ({ problem }) => {
 						{/* Constraints */}
 						<div className='my-5'>
 							<div className='text-white text-sm font-medium'>Constraints:</div>
-							<ul className='text-white ml-5 list-disc'>
-								<li className='mt-2'>
-									<code>2 ≤ nums.length ≤ 10</code>
-								</li>
-
-								<li className='mt-2'>
-									<code>-10 ≤ nums[i] ≤ 10</code>
-								</li>
-								<li className='mt-2'>
-									<code>-10 ≤ target ≤ 10</code>
-								</li>
-								<li className='mt-2 text-sm'>
-									<strong>Only one valid answer exists.</strong>
-								</li>
+							<ul className='text-white ml-5 list-disc '>
+								<div dangerouslySetInnerHTML={{ __html: problem.constraints }} />
 							</ul>
 						</div>
 					</div>
