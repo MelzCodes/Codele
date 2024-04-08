e9fd9f53b195f29254f5210896d29b1adea5ea8f
MelzCodes <matteo.melis2012@gmail.com>
2024-04-08 23:03:39 +0100

    Adding SSG to render problem page and dynamically updating the problem description component from local database

--- src/components/Workspace/Workspace.tsx
+++ src/components/Workspace/Workspace.tsx
@@ -2,15 +2,16 @@ import React from 'react';
 import ProblemDescription from './ProblemDescription/ProblemDescription';
 import PlayGround from './PlayGround/PlayGround';
 import Split from 'react-split'
+import { Problem } from '@/utils/types/problem';
 
 type WorkspaceProps = {
-    
+    problem: Problem;
 };
 
-const Workspace:React.FC<WorkspaceProps> = () => {
+const Workspace:React.FC<WorkspaceProps> = ({ problem }) => {
     
     return <Split className='split' minSize={0}>
-        <ProblemDescription />
+        <ProblemDescription problem={problem} />
         <PlayGround />
     </Split>
 }
