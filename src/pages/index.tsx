d05416ac1ca9c2073975f1dd34c63e3d61c38732
MelzCodes <matteo.melis2012@gmail.com>
2024-04-11 23:50:43 +0100

    Navigating user to a new challenge each new day

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,35 @@
 import Navbar from '@/components/Navbar/Navbar';
-import React from 'react';
+import { useUser } from '@clerk/clerk-react'
+import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
+import { problems } from '@/utils/problems';
 
 const HomePage: React.FC = () => {
+  const { user, isLoaded } = useUser();
+  const [activeProblemId, setActiveProblemId] = useState<string | null>(null);
+
+  //dynamically update the link to the new problem based on what day it is
+  useEffect(() => {
+    const fetchActiveProblemId = async () => {
+      const currentDate = new Date();
+      const lastAccessedDate = localStorage.getItem('lastAccessedDate');
+      let lastAccessedIndex = parseInt(localStorage.getItem('lastAccessedIndex') ?? '0');
+
+      if (!lastAccessedDate || new Date(lastAccessedDate).getDate() !== currentDate.getDate()) {
+          lastAccessedIndex = (lastAccessedIndex + 1) % Object.keys(problems).length;
+          localStorage.setItem('lastAccessedDate', currentDate.toString());
+          localStorage.setItem('lastAccessedIndex', lastAccessedIndex.toString());
+      }
+
+      const problemKeys = Object.keys(problems);
+      const currentProblemKey = problemKeys[lastAccessedIndex];
+      setActiveProblemId(currentProblemKey); 
+    };
+
+    fetchActiveProblemId();
+  }, []);
+
   return (
     <div className="bg-black text-white min-h-screen flex flex-col">
       <Navbar />
@@ -16,11 +42,19 @@ const HomePage: React.FC = () => {
           Imagine wordle but for all of us nerds... 
           Hold yourself accountable and solve a fun (kinda) challenge everyday with us!
         </p>
-        <Link href="/sign-up">
+        {isLoaded && user ? (
+          <Link href={`/daily-problem/${activeProblemId}`}>
           <button className="bg-blue-500 text-white px-6 py-3 rounded-md text-lg font-medium hover:bg-blue-600">
-            Get Started
+            See Daily Challenge!
           </button>
         </Link>
+        ) : (
+          <Link href="/sign-up">
+          <button className="bg-blue-500 text-white px-6 py-3 rounded-md text-lg font-medium hover:bg-blue-600">
+            Sign Up
+          </button>
+        </Link>
+        )}
       </div>
     </div>
   );
